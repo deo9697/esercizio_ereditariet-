6e8176e49c18c8edb4d745eb77374e00cc991cda
d.deOliveira <deo9674@gmail.com>
2024-04-15 10:01:55 +0200

    esercizio ereditarietà2

--- src/ereditarietà2/ex2.java
+++ src/ereditarietà2/ex2.java
@@ -1,5 +1,5 @@
 package ereditarietà2;/*Scrivere un programma che contenga una classe chiamata Forma ed un metodo chiamato
- calcolaArea() che stampi l'area della ereditarietà2.forma.
+ calcolaArea() che stampi l'area della forma.
 Crea poi una sottoclasse chiamata Rettangolo che fa override del metodo calcolaArea()
  per calcolare l'area del ereditarietà2.ex2.rettangolo.*/
 
